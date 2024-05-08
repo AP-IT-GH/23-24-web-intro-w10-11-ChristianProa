a8412fb6a4efdeba5cd0bcbb74a46ce6063aeda6
ChristianProa <christoforoo@gmail.com>
2024-05-08 20:04:41 +0200

    versi 12

--- js/script-clients.js
+++ js/script-clients.js
@@ -1,15 +1,10 @@
-/* Haal 6 pseudo-gebruikers op met behulp van de fetch-methode.
+/*Haal 6 pseudo-gebruikers op met behulp van de fetch-methode.
 Gebruik hiervoor de https://randomuser.me/ API.
 Geef voor elke user zijn/haar profiel foto (large), volledige naam en locatie weer.
 */
 
 // array met merken van koffiebonen
-const brands = ["Illy",
-    "Gran Maestro Italiano",
-    "Segafredo",
-    "Lavazza",
-    "Starbucks",
-    "Fairtrade Original"];
+const brands = ["Illy", "Gran Maestro Italiano", "Segafredo", "Lavazza", "Starbucks", "Fairtrade Original"];
 // array met merken van koffiebonen
 function random_item(brands) {
     return brands[Math.floor(Math.random() * brands.length)];
@@ -41,12 +36,12 @@ fetch('https://randomuser.me/api/?results=6')
             const user = response.results[i];
             html += `<div class="card col-12 col-sm-6 col-md-4">
 
-			<img src="${user.picture.large}" class="card-img-top" alt="foto van${user.name.first}">
+			<img src="${user.picture.large}" class="card-img-top" alt="foto van ${user.name.first} ${user.name.last}">
 
 			<div class="card-body">
-			<p class="card-title text-uppercase fs-6 fw-bolder pt-3"> ${user.name.last} </p>
-			<p class="card-text h6 small mt-2">Ik kom uit  ${user.location.country} en ben fan van de koffiebonen van ${brand}!</p>
-			<a href="mailto:${user.email}     ">
+			<p class="card-title text-uppercase fs-6 fw-bolder pt-3">${user.name.first} ${user.name.last}</p>
+			<p class="card-text h6 small mt-2">Ik kom uit ${user.location.country} en ben fan van de koffiebonen van ${random_item(brands)}!</p>
+			<a href="mailto:${user.email}">
 			  <i class="bi bi-envelope koffiebruin fs-3"></i>
 			</a>
             </div>
